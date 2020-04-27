9a5599ad6a667e85c0d24feff51db4873ea14418
isaza00 <isaza00@hotmail.com>
2020-04-27 13:27:30 -0500

    100

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+/**
+ * check_cycle - check if a list has a cycle
+ * @list: param list
+ * Return: 1 if has a cycle 0 otherwise
+ */
+int check_cycle(listint_t *list);
+{
+    listint_t *new;
+
+
+}
