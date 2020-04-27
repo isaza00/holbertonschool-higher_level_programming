3a3ac7e88b00b6d5e0be315209e4a48f6d363852
isaza00 <isaza00@hotmail.com>
2020-04-27 17:10:45 -0500

    10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,11 +1,22 @@
+#include "lists.h"
 /**
  * check_cycle - check if a list has a cycle
  * @list: param list
  * Return: 1 if has a cycle 0 otherwise
  */
-int check_cycle(listint_t *list);
+int check_cycle(listint_t *list)
 {
-    listint_t *new;
+	listint_t *slower, *faster;
 
-
+	slower = list;
+	faster = list->next;
+	while (1)
+	{
+		if (!faster || !faster->next)
+			return (0);
+		else if (faster == slower || faster->next == slower)
+			return (1);
+		slower = slower->next;
+		faster = faster->next->next;
+	}
 }
