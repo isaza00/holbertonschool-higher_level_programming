714ce22df2aa1068479c7cd005a6a24df2926069
isaza00 <isaza00@hotmail.com>
2020-04-28 14:50:09 -0500

    fixing 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print("{}{}".format(i, j), sep="", end=" ")
+            print("{}{}".format(i, j), sep="")
         else:
             print("{}{}".format(i, j), sep="", end=", ")
-print()
