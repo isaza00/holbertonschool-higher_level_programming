94a883787cda16a1607739b18e1338b906311e84
isaza00 <isaza00@hotmail.com>
2020-05-05 13:26:00 -0500

    fixing 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        if i:
-            print(*i)
+    for i in range(len(matrix)):
+        sub_len = len(matrix[i])
+        for j in range(subm_len):
+            if j != sub_len - 1:
+                end_char = ' '
+            else:
+                end_char = ''
+            print("{:d}".format(matrix[i][j]), end=end_char)
+        print("")
