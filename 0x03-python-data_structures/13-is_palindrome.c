b80145923d31e93c03e07cd5236efbd3ebdaef3c
isaza00 <isaza00@hotmail.com>
2020-05-05 17:18:59 -0500

    eficiency

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,8 +9,8 @@
 int is_palindrome(listint_t **head)
 {
 	size_t len = 0;
-	int i = 0, node_a = 0, node_b = 0, index_a = 0, index_b = 0;
-	const listint_t *temp;
+	int i = 0, j = 0, node_a = 0, node_b = 0, index_a = 0, index_b = 0;
+	const listint_t *temp, *last_loc;
 
 	if (!*head)
 		return (1);
@@ -29,17 +29,23 @@ int is_palindrome(listint_t **head)
 		while (i < index_b)
 		{
 			if (i == index_a)
+			{
 				node_a = temp->n;
+				last_loc = temp;
+			}
 			temp = temp->next;
 			i++;
 		}
 		node_b = temp->n;
-		i = 0;
+		
+		printf("node_a %d and node_b %d\n", node_a, node_b);
 		if (node_a != node_b)
 			return (0);
+		j++;
+		i = j;
 		index_a++;
 		index_b--;
-		temp = *head;
+		temp = last_loc->next;
 	}
 	return (1);
 }
