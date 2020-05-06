4cd86e43ad7f39f9e2a3286e9785ceaaa6df8ef2
isaza00 <isaza00@hotmail.com>
2020-05-05 19:27:46 -0500

    recursive

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,19 +2,21 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * is_palindrome_2 - check if a list is palindrome
+ * pal - check if a list is palindrome
  * @arr: pointer to list
  * @index_a: primero indice (inicial)
  * @index_b: seg indice (final)
  * Return: return 1 if list is palindrome or 0 otherwise
  */
-int is_palindrome_2(int *arr, int index_a, int index_b)
+int pal(int *arr, int index_a, int index_b)
 {
+	if (index_a > index_b)
+		return (1);
 	if (arr[index_a] != arr[index_b])
+	{
 		return (0);
-	if (index_a < index_b)
-		is_palindrome_2(arr, index_a + 1, index_b - 1);
-	return (1);
+	}
+	return (pal(arr, index_a + 1, index_b - 1));
 }
 /**
  * is_palindrome - check if a single linked list is palindrome
@@ -46,7 +48,7 @@ int is_palindrome(listint_t **head)
 		arr[i] = temp->n;
 		temp = temp->next;
 	}
-	if (is_palindrome_2(arr, 0, len - 1))
+	if (pal(arr, 0, len - 1) == 1)
 		return (1);
 	return (0);
 }
