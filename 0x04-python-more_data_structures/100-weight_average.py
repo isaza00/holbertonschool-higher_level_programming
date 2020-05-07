c8bcc92805329560549f04b0b04a3628160961e9
isaza00 <isaza00@hotmail.com>
2020-05-07 15:32:00 -0500

    100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -8,10 +8,7 @@ def weight_average(my_list=[]):
     for i in my_list:
         mult += i[0] * i[1]
         sum += i[1]
-        res = (mult / sum)
-    return (truncate(res, 2))
-
-
-def truncate(n, decimals=0):
-    multiplier = 10 ** decimals
-    return int(n * multiplier) / multiplier
+        if sum == 0:
+            return 0
+        res = int((mult / sum)*20) / 20
+    return (res)
