a548855d0e8fa6e934d6f6b40cb2284a463b2540
isaza00 <isaza00@hotmail.com>
2020-05-07 13:45:18 -0500

    fixing 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_d = {}
     if a_dictionary:
         for x in a_dictionary:
-            a_dictionary[x] *= 2
-    return (a_dictionary)
+            new_d[x] = a_dictionary[x] * 2
+    return (new_d)
