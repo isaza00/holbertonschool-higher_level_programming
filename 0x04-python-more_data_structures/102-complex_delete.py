45a80b4dbebb6e1eaba85298479863bcd9fcba10
isaza00 <isaza00@hotmail.com>
2020-05-07 15:47:35 -0500

    102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!u/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if a_dictionary:
+        for x in a_dictionary.copy():
+            if a_dictionary[x] == value:
+                del a_dictionary[x]
+    return (a_dictionary)
