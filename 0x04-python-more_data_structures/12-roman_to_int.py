e781625eda5055c620aaad5ceb8c1fc42d9b8e7f
isaza00 <isaza00@hotmail.com>
2020-05-07 19:36:04 -0500

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dict = {"I": 1, "V":5, "X":10, "L":50, "C":100, "D":500}
-    
+    d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    suma = 0
+    sign = 1
+    lista = []
+    for x in roman_string[::-1]:
+        if x in d:
+            lista.append(d[x])
+    for index, i in enumerate(lista[:-1]):
+        if i <= lista[index + 1]:
+            print(i, lista[index + 1])
+            suma += i * sign
+            sign = 1
+        else:
+            suma += i * sign
+            sign *= -1
+    suma += lista[-1]
+    return (suma)
