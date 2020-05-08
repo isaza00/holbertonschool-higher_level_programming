d1bc0af72a65dbd4c93ca0c432834b55b4da1be3
isaza00 <isaza00@hotmail.com>
2020-05-07 19:40:21 -0500

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string = None:
+        return (0)
     d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     suma = 0
     sign = 1
