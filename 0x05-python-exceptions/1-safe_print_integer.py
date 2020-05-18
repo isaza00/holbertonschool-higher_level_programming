d6f8050c9ff481cd157073ca0c2fac7408b0dfc3
Norman <isaza00@hotmail.com>
2020-05-18 12:27:21 -0500

    fixing 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         value = True
-    except (TypeError, ValueError)
-        value = False
+    except ValueError:
+        value = True
     return (value)
