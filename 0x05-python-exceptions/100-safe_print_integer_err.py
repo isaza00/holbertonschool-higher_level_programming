f45d1fa76280ad933e2cc5c315ea0f4deaeb883a
Norman <isaza00@hotmail.com>
2020-05-18 12:11:50 -0500

    0x05

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        value = True
+    except (TypeError, ValueError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        value = False
+
+    return (value)
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    result = 0
+    for i in range(max(len(my_list_1), len(my_list_2))):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            result = 0
+            print("division by 0")
+            continue
+        except TypeError:
+            print("wrong type")
+            result = 0
+            continue
+        except IndexError:
+            result = 0
+            print("out of range")
+            continue
+        finally:
+            new_list.append(result)
+    return (new_list)
