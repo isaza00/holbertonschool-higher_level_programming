c57d2b30ced696744df223468eb775731653dbd2
Norman <isaza00@hotmail.com>
2020-05-19 17:31:55 -0500

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,12 +48,12 @@ class Square:
     @position.setter
     def position(self, value):
         """The summary line for a class docstring should fit on one line."""
+        str = "position must be a tuple of 2 positive integers"
         if (type(value) is not tuple or len(value) != 2 or
                 type(value[0]) is not int or
                 type(value[1]) is not int or
                 value[0] < 0 or value[1] < 0):
-                raise TypeError("position must be a tuple of \
-                    2 positive integers")
+                raise TypeError(str)
         else:
             self.__position = value
 
