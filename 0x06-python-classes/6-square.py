c65154a1bfc3bf0d4956fca07399e30441159933
Norman <isaza00@hotmail.com>
2020-05-19 17:23:23 -0500

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 
 
-"""Example Google style docstrings."""
+"""The summary line for a class docstring should fit on one line.
+
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. Alternatively, attributes may be documented
+    inline with the attribute's declaration (see __init__ method below).
+
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
+
+    Attributes:
+        attr1 (str): Description of `attr1`.
+
+    """
 
 
 class Square:
