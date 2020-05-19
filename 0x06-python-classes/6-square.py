604bb3c2bdadbc6920b53d5fed215692b99018b2
Norman <isaza00@hotmail.com>
2020-05-19 16:18:36 -0500

    fixing 1 an 2

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,24 +14,24 @@ class Square:
 
     def area(self):
         """The summary line for a class docstring should fit on one line."""
-        return (self._size * self._size)
+        return (self.__size * self.__size)
 
     @property
     def size(self):
         """The summary line for a class docstring should fit on one line."""
-        return self._size
+        return self.__size
 
     @property
     def position(self):
         """The summary line for a class docstring should fit on one line."""
-        return self._position
+        return self.__position
 
     @size.setter
     def size(self, value):
         """The summary line for a class docstring should fit on one line."""
         if type(value) == int:
             if value >= 0:
-                self._size = value
+                self.__size = value
             else:
                 raise ValueError("size must be >= 0")
         else:
@@ -42,7 +42,7 @@ class Square:
         """The summary line for a class docstring should fit on one line."""
         if (type(value) == tuple and len(value) == 2 and
                 type(value[0]) == int and type(value[1]) == int):
-                self._position = value
+                self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
 
@@ -50,17 +50,17 @@ class Square:
         """The summary line for a class docstring should fit on one line."""
         i, j = 0, 0
         x, y = 0, 0
-        if self._size == 0:
+        if self.__size == 0:
             print()
         else:
-            while y < self._position[1]:
+            while y < self.__position[1]:
                 print()
                 y += 1
-            while i < self._size:
-                while x < self._position[0]:
+            while i < self.__size:
+                while x < self.__position[0]:
                     print(" ", end="")
                     x += 1
-                while j < self._size:
+                while j < self.__size:
                     print("#", end="")
                     j += 1
                 print()
