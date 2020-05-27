a45739b7c6e00fa02bae0a3db69b70b7e33caa54
Norman <isaza00@hotmail.com>
2020-05-26 19:43:52 -0500

    100 y 101

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(a = []):
+def magic_string(a=[]):
     a.append("Holberton")
     return ", ".join(a)
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,5 +4,4 @@
 
 class LockedClass:
     """ class that just allow attr called first_name """
-    
     __slots__ = ["first_name"]
