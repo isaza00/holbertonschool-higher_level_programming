fd983053a744da1daabf036e226cc1abea241157
Norman <isaza00@hotmail.com>
2020-07-13 16:46:43 -0500

    8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
- -- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
- -- cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+-- cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SELECT COUNT(*) FROM first_table WHERE id = 89;
