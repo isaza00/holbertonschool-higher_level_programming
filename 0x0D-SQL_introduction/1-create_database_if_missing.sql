f0387dacf4f6e95f0207e3d002c6adf6ea56e3f3
Norman <isaza00@hotmail.com>
2020-07-13 16:42:38 -0500

    1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
--- create database
+-- create database hbtn_0c_0
 -- usage cat 1-create_database_if_missing.sql | mysql -hlocalhost -uroot -p
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
