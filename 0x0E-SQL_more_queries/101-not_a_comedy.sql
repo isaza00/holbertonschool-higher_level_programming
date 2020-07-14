a77f01e6c957e32c0fecac6d3ac96635d309c35e
Norman <isaza00@hotmail.com>
2020-07-14 15:06:37 -0500

    101

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,3 @@
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+-- cat 101-not_a_comedy.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+SELECT DISTINCT tv_shows.title FROM tv_show_genres RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id LEFT OUTER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id where tv_shows.title NOT IN (SELECT tv_shows.title FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = "Comedy") ORDER BY tv_shows.title;
