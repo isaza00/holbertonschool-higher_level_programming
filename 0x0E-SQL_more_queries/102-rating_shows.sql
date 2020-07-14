4858757e750cb7f931d9c81347f79ea1e565c01e
Norman <isaza00@hotmail.com>
2020-07-14 15:43:47 -0500

    102

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,3 @@
+--  lists all shows from hbtn_0d_tvshows_rate by their rating
+-- cat 102-rating_shows.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_show_ratings INNER JOIN tv_shows ON tv_show_ratings.show_id = tv_shows.id GROUP BY tv_shows.title ORDER BY rating DESC;
