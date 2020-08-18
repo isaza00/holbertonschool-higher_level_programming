ac7cea995887422b9f93b2989908c9de92584900
Norman <isaza00@hotmail.com>
2020-08-18 14:28:27 -0500

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,8 +14,8 @@ def find_recursively(lista, low, high, length):
     """ find peak recursively """
     mid = (low + high)/2
     mid = int(mid)
-    if mid == 0 or lista[mid - 1] <= lista[mid]\
-            and mid == (length - 1) or lista[mid + 1] <= lista[mid]:
+    if (mid == 0 or lista[mid - 1] <= lista[mid])\
+            and (mid == (length - 1) or lista[mid + 1] <= lista[mid]):
                 return lista[mid]
     elif mid > 0 and lista[mid - 1] > lista[mid]:
         return find_recursively(lista, low, mid - 1, length)
