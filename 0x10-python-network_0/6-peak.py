6fa07297a2a8d56b602e62abb0765db76972a137
Norman <isaza00@hotmail.com>
2020-08-18 14:23:37 -0500

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,2 +1,25 @@
-
+#!/usr/bin/python3
 """ find min number of a list """
+
+
+def find_peak(list_of_integers):
+    """ call recursive function """
+    if not list_of_integers:
+        return
+    length = len(list_of_integers)
+    return find_recursively(list_of_integers, 0, length - 1, length)
+
+
+def find_recursively(lista, low, high, length):
+    """ find peak recursively """
+    print("low", low, "high", high)
+    mid = (low + high)/2
+    mid = int(mid)
+    print("mid", mid)
+    if mid == 0 or lista[mid - 1] <= lista[mid]\
+    and mid == (length - 1) or lista[mid + 1] <= lista[mid]:
+        return lista[mid]
+    elif mid > 0 and lista[mid - 1] > lista[mid]:
+        return find_recursively(lista, low, mid - 1, length)
+    else:
+        return find_recursively(lista, mid + 1, high, length)
