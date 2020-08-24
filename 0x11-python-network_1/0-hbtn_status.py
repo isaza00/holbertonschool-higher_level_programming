2f60800d6b4068679f8777b24e2667a8d48c033f
Norman <isaza00@hotmail.com>
2020-08-24 14:07:50 -0500

    0 y 4

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
 
-    print("Body response:\n\t- type: {}\
-        \n\t- content: {}\
-        \n\t- utf8 content: {}".format(type(html), html, html.decode("utf-8")))
+    print("Body response:\n\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("utf-8")))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\
-        \n\t- content: {}".format(type(r.text), r.text))
+    print("Body response:\n\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
