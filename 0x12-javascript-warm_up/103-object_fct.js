1d996d2b3a7b4521e33788c085796b24b5ea7ab9
Norman <isaza00@hotmail.com>
2020-09-15 18:19:24 -0500

    103

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// update this script adding a new function incr that increments value
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value += 1;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
