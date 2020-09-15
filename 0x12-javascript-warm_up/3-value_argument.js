9b0b5af9f92677ec1211ce3802e7b509269b4a71
Norman <isaza00@hotmail.com>
2020-09-15 12:15:29 -0500

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints first argument passed
 const args = process.argv.slice(2);
-if (args.length === 0) {
+if (args[0] === undefined) {
   console.log('No argument');
 } else {
   console.log(args[0]);
