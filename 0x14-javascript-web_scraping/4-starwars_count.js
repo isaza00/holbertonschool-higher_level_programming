89a04265ad85c3bfade5fe2ba04b65eb78ec54f9
Norman <isaza00@hotmail.com>
2020-09-22 13:16:34 -0500

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,13 @@
 // print number of films of character 18
 const request = require('request');
 const url = process.argv[2];
+const character = 'https://swapi-api.hbtn.io/api/people/18/';
 let sum = 0;
 request(url, function (error, response, body) {
   if (error) return console.error('error:', error);
   for (const item of JSON.parse(body).results) {
-    for (const uri of item.characters) {
-      if (uri === 'https://swapi-api.hbtn.io/api/people/18/') {
-        sum += 1;
-      }
+    if (item.characters.includes(character)) {
+      sum += 1;
     }
   }
   console.log(sum);
